e082fc3decd06545ecfa68d435402ec7d0047d90
sarang682 <jms20914@gmail.com>
2023-11-28 17:01:05 +0900

    2. 시큐리티 설정

---
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests((authorizeRequests ->
+                        authorizeRequests
+                            .requestMatchers("/user/**").authenticated()
+                            .requestMatchers("/manager/**").hasAnyRole("MANAGER","ADMIN")
+                            .requestMatchers("/admin/**").hasRole("ADMIN")
+                            .anyRequest().permitAll()
+                 ))
+                .formLogin((formLogin) ->
+                        formLogin.loginPage("/login")
+                );
+
+        return http.build();
+    }
+}
--- src/main/java/com/example/springsecurity/controller/IndexController.java
+++ src/main/java/com/example/springsecurity/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.example.springsecurity.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -11,4 +12,33 @@ public class IndexController {
         return "index";
     }
 
+    @GetMapping("/user")
+    public @ResponseBody String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    public @ResponseBody String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원가입 완료";
+    }
 }
